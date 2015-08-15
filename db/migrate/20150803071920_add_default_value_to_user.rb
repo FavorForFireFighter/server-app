8c3c264f8700bf3bba9bffa57e786d8e7c3f13c5
Taro Matsuzawa <taro@georepublic.co.jp>
2015-08-15 14:45:41 +0900

    fix: fail when db:rollback

--- db/migrate/20150803071920_add_default_value_to_user.rb
+++ db/migrate/20150803071920_add_default_value_to_user.rb
@@ -1,5 +1,9 @@
 class AddDefaultValueToUser < ActiveRecord::Migration
-  def change
-    change_column :users, :admin_flag,:boolean, default: false
+  def up
+    change_column :users, :admin_flag, :boolean, default: false
+  end
+
+  def down
+    change_column :users, :admin_flag, :boolean
   end
 end
