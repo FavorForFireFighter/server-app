a3075c4740d9587a278f23e229cfefcabb7ca150
Yasuaki KAWAKAMI <bougan1160@gmail.com>
2015-08-24 18:19:55 +0900

    remove account confirmation through email and implement soft_delete

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -4,7 +4,6 @@ FactoryGirl.define do
     password {Faker::Internet.password(8)+"a1"}
     username {Faker::Internet.user_name}
     admin_flag false
-    confirmed_at Date.today
 
     factory :admin do
       admin_flag true
