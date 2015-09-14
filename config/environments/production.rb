d200a0f7f6f1db75e3796c5d646de12516f1553f
Taro Matsuzawa <taro@georepublic.co.jp>
2015-09-14 13:09:41 +0900

    revert s3 bucket name

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,7 +81,7 @@ Rails.application.configure do
       :storage => :s3,
       :s3_host_name => 's3-ap-northeast-1.amazonaws.com',
       :s3_credentials => {
-          :bucket => 'crowdbus'
+          :bucket => 'crowdb.us'
       }
   }
 end
