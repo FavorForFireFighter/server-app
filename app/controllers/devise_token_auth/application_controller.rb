2093673cec7df09b706e46b4dbbfa06503b5534f
Yasuaki KAWAKAMI <bougan1160@gmail.com>
2015-12-14 11:36:53 +0900

    add missed files

---
+++ app/controllers/devise_token_auth/application_controller.rb
@@ -0,0 +1,18 @@
+module DeviseTokenAuth
+  class ApplicationController < DeviseController
+    protect_from_forgery with: :null_session
+    include DeviseTokenAuth::Concerns::SetUserByToken
+
+    protected
+
+    def resource_class(m=nil)
+      if m
+        mapping = Devise.mappings[m]
+      else
+        mapping = Devise.mappings[resource_name] || Devise.mappings.values.first
+      end
+
+      mapping.to
+    end
+  end
+end
